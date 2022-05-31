ab9447cbdea33eb91395116614603a7a996aa3c9
Sebastian Kuehnau <seb@vaadin.com>
2022-05-31 16:20:47 +0200

    add a customer ResponseErrorHandler

--- client/src/main/java/foo/v5archstudygroup/exercises/backpressure/client/RestClient.java
+++ client/src/main/java/foo/v5archstudygroup/exercises/backpressure/client/RestClient.java
@@ -2,11 +2,15 @@ package foo.v5archstudygroup.exercises.backpressure.client;
 
 import foo.v5archstudygroup.exercises.backpressure.messages.Messages;
 import foo.v5archstudygroup.exercises.backpressure.messages.converter.ProcessingRequestMessageConverter;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.io.IOException;
 import java.net.URI;
 import java.util.List;
 
@@ -26,20 +30,24 @@ public class RestClient {
         requestFactory.setReadTimeout(1000);
         restTemplate = new RestTemplate(List.of(new ProcessingRequestMessageConverter()));
         restTemplate.setRequestFactory(requestFactory);
+        restTemplate.setErrorHandler(new BackpressureResponseErrorHandler());
         this.serverUri = serverUri;
     }
 
     public void sendToServer(Messages.ProcessingRequest processingRequest) {
         var uri = UriComponentsBuilder.fromUri(serverUri).path("/process").build().toUri();
-        var throwException = true ;
 
-        while (throwException) {
-            throwException = false ;
-            try {
-                restTemplate.postForEntity(uri, processingRequest, Void.class);
-            } catch (Exception ex) {
-                throwException = true ;
-            }
+        ResponseEntity<Void> booleanResponseEntity ;
+
+        do {
+            booleanResponseEntity = restTemplate.postForEntity(uri, processingRequest, Void.class);
+        } while(!booleanResponseEntity.getStatusCode().equals(HttpStatus.TOO_MANY_REQUESTS));
+    }
+
+    private class BackpressureResponseErrorHandler extends DefaultResponseErrorHandler {
+        @Override
+        public boolean hasError(ClientHttpResponse response) throws IOException {
+            return response.getStatusCode().equals(HttpStatus.TOO_MANY_REQUESTS) ? false : super.hasError(response) ;
         }
     }
 }
--- server/src/main/java/foo/v5archstudygroup/exercises/backpressure/server/controller/DataProcessingController.java
+++ server/src/main/java/foo/v5archstudygroup/exercises/backpressure/server/controller/DataProcessingController.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.client.HttpServerErrorException;
 
 /**
  * This is the REST controller that receives requests from the client and forwards them to the {@link DataProcessor}.
@@ -26,6 +25,6 @@ public class DataProcessingController {
     public ResponseEntity<Void> process(@RequestBody Messages.ProcessingRequest request) {
         return dataProcessor.enqueue(request) ?
             ResponseEntity.ok(null) :
-            ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).body(null);
+            ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
     }
 }
